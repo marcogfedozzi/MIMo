ae9d8506df9b2351ca68757915a09c31aa63d09b
hypothe <marco.fedozzi@iit.it>
2025-04-08 14:13:26 +0200

    <Edit> handle "fake" camera names for logpolar

--- mimoVision/vision.py
+++ mimoVision/vision.py
@@ -24,6 +24,8 @@ try:
 except NameError:
     import cv2 as cv
 
+from abc import ABC
+
 class Vision:
     """ Abstract base class for vision.
 
@@ -319,7 +321,7 @@ class SimpleVision(Vision):
 
 
 class EditVision(SimpleVision):
-    """A class that edits the iamges returned by the cameras before returning them to the environment.
+    """A class that edits the images returned by the cameras before returning them to the environment.
 
     Args:
         env: The environment to which this module should be attached
@@ -353,8 +355,11 @@ class EditVision(SimpleVision):
             imgs[camera] = self._image_warp_func[camera](img)
         
         return imgs
+    
+class ILogPolarVision(ABC):
+    pass
 
-class LogPolarVision(EditVision):
+class LogPolarVision(EditVision, ILogPolarVision):
     """
     Like the SimpleVision class, but the image is transformed into
     logpolar coordinates before being returned.
@@ -378,39 +383,44 @@ class LogPolarVision(EditVision):
         self.camera_transform_parameters = {}
 
         for camera, params in camera_parameters.items():
-            max_radius = params.get("maxRadius", min(params["width"] / 2, params["height"] / 2))
-            log_fraction = params.get("logFraction", 1)
-
-            self.camera_transform_parameters[camera] = {
-                'Klog': params["width"]*log_fraction / np.log(max_radius),
-                'Kangle': params["height"] / (2*np.pi),
-                'xc': params["width"] / 2,
-                'yc': params["height"] / 2,
-            }
-            
-            params.update(
-                dict(
-                    warp_function=cv.warpPolar,
-                    warp_function_args=dict(
-                        maxRadius=max_radius,
-                        dsize=(int(params["width"]*log_fraction), params["height"]),
-                        center=(params["width"] / 2, params["height"] / 2),
-                        flags=cv.INTER_LINEAR + cv.WARP_FILL_OUTLIERS + cv.WARP_POLAR_LOG
-                    ), # arguments to be passed to the function
-
-                    dewarp_function=cv.warpPolar,
-                    dewarp_function_args=dict(
-                        maxRadius=max_radius,
-                        dsize=(params["width"], params["height"]),
-                        center=(params["width"] / 2, params["height"] / 2),
-                        flags=cv.WARP_FILL_OUTLIERS + cv.WARP_POLAR_LOG + cv.WARP_INVERSE_MAP
-                    )
-                )
-            )
-        
-        
+            out_params, self.camera_transform_parameters[camera] = self._get_logpolar_params(camera, params)
+            params.update(out_params)
 
         super().__init__(env, camera_parameters)
+    
+    def _get_logpolar_params(self, camera, params):
+        out_params= dict(params)
+
+        max_radius = out_params.get("maxRadius", min(out_params["width"] / 2, out_params["height"] / 2))
+        log_fraction = out_params.get("logFraction", 1)
+
+        camera_transform_parameters = {
+            'Klog': out_params["width"]*log_fraction / np.log(max_radius),
+            'Kangle': out_params["height"] / (2*np.pi),
+            'xc': out_params["width"] / 2,
+            'yc': out_params["height"] / 2,
+        }
+        
+        out_params.update(
+            dict(
+                warp_function=cv.warpPolar,
+                warp_function_args=dict(
+                    maxRadius=max_radius,
+                    dsize=(int(out_params["width"]*log_fraction), out_params["height"]),
+                    center=(out_params["width"] / 2, out_params["height"] / 2),
+                    flags=cv.INTER_LINEAR + cv.WARP_FILL_OUTLIERS + cv.WARP_POLAR_LOG
+                ), # arguments to be passed to the function
+
+                dewarp_function=cv.warpPolar,
+                dewarp_function_args=dict(
+                    maxRadius=max_radius,
+                    dsize=(out_params["width"], out_params["height"]),
+                    center=(out_params["width"] / 2, out_params["height"] / 2),
+                    flags=cv.WARP_FILL_OUTLIERS + cv.WARP_POLAR_LOG + cv.WARP_INVERSE_MAP
+                )
+            )
+        )
+        return out_params, camera_transform_parameters
     
     def get_3D_point(self, rho, phi, camera_name): 
         
@@ -445,26 +455,127 @@ class LogPolarVision(EditVision):
         """
         return self._image_dewarp_func[camera_name](image)
 
-    
     @property
     def width(self) -> int:
         """ Returns the width of the camera with name camera_name. """
         return {k: int((_v.width - _v.left) * self.camera_parameters[k]['logFraction']) for k, _v in self._viewports.items()}
     
-class LogPolarNCartesianVision(LogPolarVision):
+class LogPolarNCartesianVision(SimpleVision, ILogPolarVision):
     """
     Returns both the logpolar and the original cartesian images.
     """
+    
+    def __init__(self, env, camera_parameters): #, func: Callable[[np.ndarray, Dict], np.ndarray] = lambda x, _: x, func_args: Dict = {}):
+
+        cartesian_camera_parameters = {}
+        logpolar_camera_parameters = {}
+        self.is_camera_logpolar = {}
+
+        self.l2c_cameramap = {} # map logpolar camera names used for convenience to real camera sensor names
+        self.c2l_cameramap = {} # map logpolar camera names used for convenience to real camera sensor names
+
+        for camera, params in camera_parameters.items():
+            is_logpolar = params.pop("is_logpolar", False)
+            self.is_camera_logpolar[camera] = is_logpolar
+
+        for camera in camera_parameters: # logpolar
+            if self.is_camera_logpolar[camera]:
+                for camera2 in camera_parameters: # cartesian
+                    if self.is_camera_logpolar[camera2] == False  and camera2 in camera:
+                        self.l2c_cameramap[camera] = camera2
+                        self.c2l_cameramap[camera2] = camera
+                        self.c2l_cameramap[camera] = camera
+                        break
+            else:
+                self.l2c_cameramap[camera] = camera
+
+
+        for camera, params in camera_parameters.items():
+            is_logpolar = self.is_camera_logpolar[camera]
+            camera
+
+            if is_logpolar:
+                logpolar_camera_parameters[camera] = params
+            else:
+                cartesian_camera_parameters[camera] = params    
+
+        self.cartesian_vision   = SimpleVision(env, cartesian_camera_parameters)
+
+        remapped_logpolar_camera_parameters = {}
+        for camera, params in logpolar_camera_parameters.items():
+            remapped_logpolar_camera_parameters[self.l2c_cameramap[camera]] = params
+        self.logpolar_vision    = LogPolarVision(env, remapped_logpolar_camera_parameters)
+
+        self.camera_parameters = {}
+        self.camera_parameters.update(cartesian_camera_parameters)
+        self.camera_parameters.update(logpolar_camera_parameters)
    
     def get_vision_obs(self):
-        imgs =  super().get_vision_obs()
+        imgs = {}
 
-        for camera, img in imgs.items():
-            imgs[camera] = img
-            imgs[camera+'_lp'] = self._image_warp_func[camera](img)
-        
+        imgs.update(self.cartesian_vision.get_vision_obs())
+        for lp_camera, lp_img in self.logpolar_vision.get_vision_obs().items():
+            imgs[self.c2l_cameramap[lp_camera]] = lp_img
+
+
         return imgs
     
+    def get_cartesian_image(self, image, camera_name):
+        if self.is_camera_logpolar[camera_name]:
+            return self.logpolar_vision.get_cartesian_image(image, self.l2c_cameramap[camera_name])
+        else:
+            return self.cartesian_vision.get_cartesian_image(image, camera_name)
+        
+    def get_3D_point(self, a, b, camera_name):
+        if self.is_camera_logpolar[camera_name]:
+            return self.logpolar_vision.get_3D_point(rho=a, phi=b, camera_name=self.l2c_cameramap[camera_name])
+        else:
+            return self.cartesian_vision.get_3D_point(x=a, y=b, camera_name=camera_name)
+        
+    def get_logpolar_from_cartesian_2D(self, x, y, camera_name):
+        
+        if self.is_camera_logpolar[camera_name]:
+            return self.logpolar_vision.get_logpolar_from_cartesian_2D(x, y, self.l2c_cameramap[camera_name])
+        else:
+            raise ValueError(f"The camera {camera_name} is already in cartesian space")
+    
+    def get_2d_from_3d(self, point: NDArray, camera_name: str):
+
+        x, y = self.get_2d_from_3d(point, camera_name)
+
+        if self.is_camera_logpolar[camera_name]:
+            return self.logpolar_vision.get_logpolar_from_cartesian_2D(x, y, self.l2c_cameramap[camera_name])
+        else:
+            return x, y
+    
+
+    @property
+    def width(self) -> int:
+        """ Returns the width of the camera with name camera_name. """
+
+        w = {}
+
+        for k, _v in self.logpolar_vision._viewports.items():
+            w[k] = int((_v.width - _v.left) * self.camera_parameters[self.c2l_cameramap[k]]['logFraction'])
+        
+        
+        for k, _v in self.cartesian_vision._viewports.items():
+            w[k] = _v.width - _v.left
+
+        return w    
+    
+    
+    @property
+    def height(self) -> int:
+        """ Returns the width of the camera with name camera_name. """
+        h = {}
+
+        for vision in [self.logpolar_vision, self.cartesian_vision]:
+            for k, _v in vision._viewports.items():
+                h[k] = _v.height - _v.bottom
+
+        return h    
+
 class IncreasingActuityVision(EditVision):
     """
     Like the SimpleVision class, but the image is blurred before being returned.
